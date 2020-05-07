3cd6df53668599907b5dc693b23ec7db5657a38a
Guy Levy <gl060792@gmail.com>
2020-05-07 15:24:29 +0300

    Google sign in fix ups, DB get data still not working :(
    Signed-off-by: Guy Levy <gl060792@gmail.com>

--- ScanVentureProject/src/org/tensorflow/demo/Login.java
+++ ScanVentureProject/src/org/tensorflow/demo/Login.java
@@ -14,6 +14,7 @@ import android.os.Message;
 import android.util.DebugUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -38,8 +39,11 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Console;
 import java.io.FileDescriptor;
@@ -47,54 +51,66 @@ import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.ConsoleHandler;
 
-public class Login extends Activity implements View.OnClickListener{
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference("server/saving-data");
+public class Login extends Activity{
+
     private static final int RC_SIGN_IN = 1;
     SignInButton signInButton;
     private static final int SIGN_IN = 1;
     GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-    private String tkn;
+    DatabaseReference rootref = FirebaseDatabase.getInstance().getReference();
+    Account userData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_login);
+        //checkIfLoggedIn();
         //Set dimensions of Google's sign in button - Standard
         signInButton = findViewById(R.id.googleLoginBtn);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
         mAuth = FirebaseAuth.getInstance();
-        //Google api required classes to perform login
+        //Google api required clases to perform login
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
-        //set onClick Listener for the button
-        findViewById(R.id.googleLoginBtn).setOnClickListener(this);
     }
     @Override
-    protected void onStart() {//check if a user has already signed in to ScanVenture with Google.
+    protected void onStart() {
         super.onStart();
-        GoogleSignInAccount currentUser = GoogleSignIn.getLastSignedInAccount(this);
-        //GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if (currentUser != null) { //user has already logged in with google
-            Intent goToSettings = new Intent(Login.this,Settings.class);
-            goToSettings.putExtra("currentUser",currentUser);
-            startActivity(goToSettings);
-            finish();
-            //TODO : Additional actions to perform
+        GoogleSignInAccount user = GoogleSignIn.getLastSignedInAccount(this);
+        if(user!=null){
+            getUserDataFromDB(user);
+            Intent intent = new Intent(getApplicationContext(),Settings.class);
+            intent.putExtra("user_data",userData);
+            startActivity(intent);
         }
     }
-    @Override
-    public void onClick(View v){
-        switch (v.getId()) {
-            case R.id.googleLoginBtn:
-                signIn();
-                break;
-            //Todo : Add additional onClick functionality of other buttons if needed
-        }
+
+    private void getUserDataFromDB(final GoogleSignInAccount currentUser) {
+        DatabaseReference usersRef = rootref.child("users");
+        final String email = currentUser.getEmail();
+        final String emailName = email.substring(0, email.indexOf('@'));
+        usersRef.child(emailName).setValue(emailName);
+        usersRef.child(emailName).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userData = new Account(emailName,dataSnapshot.child("name").getValue().toString(),(int)dataSnapshot.child("level").getValue());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void signIn() {
@@ -104,7 +120,6 @@ public class Login extends Activity implements View.OnClickListener{
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -112,7 +127,7 @@ public class Login extends Activity implements View.OnClickListener{
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 Log.d("Google SignIn made!", "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
+                firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
                 Log.w("Failed to sign in!", "Google sign in failed", e);
@@ -120,44 +135,64 @@ public class Login extends Activity implements View.OnClickListener{
             }
         }
     }
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+    private void firebaseAuthWithGoogle(final GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d("User signed in!", "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                            writeNewAccountToFireBase(acct);
+                            Intent intent = new Intent(getApplicationContext(),Settings.class);
+                            intent.putExtra("user_data",userData);
+                            startActivity(intent);
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("Failed to sign in!", "signInWithCredential:failure", task.getException());
-                            //Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                            updateUI(null);
+                            Toast.makeText(Login.this, "Sorry auth failed.", Toast.LENGTH_SHORT).show();
                         }
-
                         // ...
                     }
                 });
     }
     private void updateUI(FirebaseUser user){
         if (user == null){
-            //LOGIN FAILED
+            //TODO: sign in failed handle this!
         }
         else {
             //Account loggedInUser = new Account();
-            //writeAccountToFireBase(user);
-            startActivity(new Intent(Login.this,Settings.class));
+            Intent goTo = new Intent(Login.this,Settings.class);
+            goTo.putExtra("user_data",userData);
+            startActivity(goTo);
             finish();
         }
     }
 
-    private void writeAccountToFireBase(FirebaseUser user) {
-        //Create accounts section in DB
-        DatabaseReference accountsRef = ref.child("Accounts");
+    private void writeNewAccountToFireBase(GoogleSignInAccount user) {
+        //Create accounts section in DB and get a ref to it
+        DatabaseReference usersRef = rootref.child("users");
         //Write user data as an Account class object
-        accountsRef.child(user.getEmail()).setValue(new Account(user.getUid(),user.getDisplayName(),0));
+        final String email = user.getEmail();
+        final String emailName = email.substring(0, email.indexOf('@'));
+        usersRef.child(emailName).child("name").setValue(user.getDisplayName());
+        usersRef.child(emailName).child("level").setValue(0);
+        userData = new Account(user.getEmail(),user.getDisplayName(),0); //only for a new user
+    }
+
+    private void checkIfLoggedIn() {
+        FirebaseAuth.AuthStateListener mAuthListener;
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    Intent intent = new Intent(Login.this, Settings.class);
+                    //getUserDataFromDB(user);
+                    intent.putExtra("user_data",userData);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 }
